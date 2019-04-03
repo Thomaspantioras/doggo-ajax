08fdf081b2e2b9a0468ebfb5b432a49b107460d7
thomaspantioras <t.pantioras@gmail.com>
2019-04-03 15:25:10 +0100

    added multiple doggo images

--- doggos.js
+++ doggos.js
@@ -1,1 +1,20 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const doggos = document.querySelector(".doggos");
+
+const addNewDog = () => {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(response => {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(processedResponse => {
+      const img = document.createElement("img");
+      img.src = processedResponse.message;
+      img.alt = "Cute dog";
+      doggos.appendChild(img);
+    });
+};
+
+document.querySelector(".add-dog").addEventListener("click", addNewDog);
