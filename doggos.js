2893e1330addfee991bf94c8c794899eb4436980
thomaspantioras <t.pantioras@gmail.com>
2019-04-04 21:52:49 +0100

    modified the images order

--- doggos.js
+++ doggos.js
@@ -1,5 +1,9 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+const BREEDS_LIST = "https://dog.ceo/api/breeds/list/all";
+
 const doggos = document.querySelector(".doggos");
+const breedSelector = document.querySelector(".breed-selector");
+let dogBreed;
 
 const showSpinner = () => {
   document.getElementById("loading-icon").style.display = "flex";
@@ -9,8 +13,10 @@ const showSpinner = () => {
   });
   setTimeout(() => {
     document.getElementById("loading-icon").style.display = "none";
+    // let count = 0;
     imageBoxes.forEach(box => {
       box.className = "image-box";
+      // box.style.order = count--;
     });
   }, 1000);
 };
@@ -20,6 +26,30 @@ const showSpinner = () => {
 //   document.getElementById("loading-icon").style.display = "none";
 // };
 
+const selectBreed = () => {
+  const promise = fetch(BREEDS_LIST);
+
+  promise
+    .then(response => {
+      const processingResponse = response.json();
+      return processingResponse;
+    })
+    .then(processedResponse => {
+      const breeds = Object.keys(processedResponse.message);
+      console.log(breeds);
+
+      breeds.forEach(breed => {
+        const option = document.createElement("option");
+        console.log(option);
+        option.className = "breed-name";
+        option.value = breed;
+        option.text = breed;
+        // breedSelector.appendChild(option);
+        breedSelector.add(option);
+      });
+    });
+};
+let count = 0;
 const addNewDog = () => {
   showSpinner();
 
@@ -32,7 +62,7 @@ const addNewDog = () => {
     .then(processedResponse => {
       const div = document.createElement("div");
       div.className = "image-box";
-
+      div.style.order = count--;
       const img = document.createElement("img");
       img.className = "image";
       img.src = processedResponse.message;
@@ -44,5 +74,9 @@ const addNewDog = () => {
     })
     .catch(err => console.error(err));
 };
+
+document
+  .querySelector(".breed-selector")
+  .addEventListener("click", selectBreed);
 
 document.querySelector(".add-dog").addEventListener("click", addNewDog);
